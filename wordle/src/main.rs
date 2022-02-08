2d0e2132108e679b97531555fb789edb201d03fd
sungwooHa <ttlhi10@gmail.com>
2022-02-09 02:31:42 +0900

    add game guide

--- wordle/src/main.rs
+++ wordle/src/main.rs
@@ -2,15 +2,26 @@ use std::io::BufRead;
 
 use wordle::Game;
 use std::io;
+use std::io::Write;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, this is wordle game!");
 
-    let maxRound = 10;       
+    print!("round : ");
+    io::stdout().flush().unwrap(); //flush를 통해서 print가 즉시 실행되도록, https://doc.rust-lang.org/std/macro.print.html
+    let mut game_round = String::new();
+    io::stdin().read_line(&mut game_round).expect("failed to read line");
+    let game_round : u32 = game_round.trim().parse().expect("please type a number");
+
+
     let word= String::from("Hello");
-    let mut game = Game::new(word, maxRound);
+
+    let mut game = Game::new(word, game_round);
     
+    println!("--- Game Start ---");
     loop{
+        print!("guess word : ");
+        io::stdout().flush().unwrap();
         let mut guess_word = String::new();
         let guess_word  = match io::stdin().read_line(&mut guess_word){
             Ok(_) => String::from(guess_word.trim()),
@@ -18,23 +29,21 @@ fn main() {
                 return panic!("Failed to read line");
             },
         };
-        
-
 
         match game.play(&guess_word) {
             Ok(isSuccess) => {
                 if isSuccess{
-                    println!("good success");
                     break;
 
                 }
                 else {
-                    println!("round : {} / {}" , game.get_rest_round(), maxRound);
+                    println!("round : {} / {}" , game.get_rest_round(), game_round);
                 }
             },
             Err(errMsg) => {
                 println!("{}", errMsg); 
             }
         }
+        println!("--- next round ---");
     }
 }
